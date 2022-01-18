5c64552f342151c8736aecd7302dabd17ba1b1dd
in2dam-b <in2dam-b@elorrieta.int>
2022-01-18 21:05:28 +0100

    Grid view creada

---
+++ RetoVideoclub-ASP/frmPeliculas.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="frmPeliculas.aspx.vb" Inherits="RetoVideoclub_ASP.frmPeliculas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+                <div>
+            <asp:Label ID="lblTitulo" runat="server" Font-Size="XX-Large" Text="Titulo:"></asp:Label>
+            &nbsp;<asp:TextBox ID="txtTitulo" runat="server" Width="134px" AutoCompleteType="Disabled"></asp:TextBox>
+            
+            <br />
+            <asp:Label ID="lblAnyo" runat="server" Font-Size="XX-Large" Text="Año:"></asp:Label>
+            &nbsp;<asp:TextBox ID="txtAnyo" runat="server" Width="72px"></asp:TextBox>
+            
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server"
+                    ConnectionString="<%$ ConnectionStrings:ConnectionString %>"
+                    ProviderName="<%$ ConnectionStrings:ConnectionString.ProviderName %>" 
+                    SelectCommand="SELECT [Titulo], [Anyo] ,Director, Generos.Nombre
+                    FROM [Peliculas] 
+                    INNER JOIN [Generos]
+                    ON Peliculas.CodGenero  = Generos.GeneroId
+                    WHERE ([Titulo] LIKE  ? + '%') ">
+                    <SelectParameters>
+                        <asp:ControlParameter ControlID="txtTitulo" DefaultValue="%" Name="Titulo" PropertyName="Text"
+                        Type="String" />
+                    </SelectParameters>
+              </asp:SqlDataSource>
+
+            <br />
+            <br />
+              <asp:GridView ID="GridView1" runat="server" AllowPaging="True"
+                    AutoGenerateColumns="False" CellPadding="4" DataSourceID="SqlDataSource1">
+                <Columns>
+                   
+                    <asp:BoundField DataField="Titulo" HeaderText="Titulo" SortExpression="Titulo" />
+                    <asp:BoundField DataField="Anyo" HeaderText="Anyo" SortExpression="Anyo" />
+                    <asp:BoundField DataField="Director" HeaderText="Director" SortExpression="Director" />
+                    <asp:BoundField DataField="Nombre" HeaderText="Nombre" SortExpression="Nombre" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RetoVideoclub-ASP/frmPeliculas.aspx.designer.vb
@@ -0,0 +1,78 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class frmPeliculas
+
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control lblTitulo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblTitulo As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtTitulo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtTitulo As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control lblAnyo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblAnyo As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txtAnyo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtAnyo As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control SqlDataSource1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSource1 As Global.System.Web.UI.WebControls.SqlDataSource
+
+    '''<summary>
+    '''Control GridView1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents GridView1 As Global.System.Web.UI.WebControls.GridView
+End Class
