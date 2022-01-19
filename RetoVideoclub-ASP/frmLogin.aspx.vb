e951d2d20e9e3d34f2718731fed30ed65a68ed19
in2dam-b <in2dam-b@elorrieta.int>
2022-01-19 20:47:38 +0100

    Login con conexión hecha

--- RetoVideoclub-ASP/frmLogin.aspx.vb
+++ RetoVideoclub-ASP/frmLogin.aspx.vb
@@ -2,44 +2,72 @@
 Imports System.Configuration
 Imports System.Data.SqlClient
 Imports System.Web.Security
+Imports System.Data.OleDb
+Imports System.Windows.Forms
+
 Public Class frmLogin
     Inherits System.Web.UI.Page
 
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If Not Me.Page.User.Identity.IsAuthenticated Then
-            FormsAuthentication.RedirectToLoginPage()
-
-        End If
-    End Sub
+    Private _OleDbConnection As OleDbConnection
+    Public Property OleDbConnection() As OleDbConnection
+        Get
+            Return _OleDbConnection
+        End Get
+        Set(ByVal value As OleDbConnection)
+            _OleDbConnection = value
+        End Set
+    End Property
 
     Protected Sub MiLogin_Authenticate(sender As Object, e As AuthenticateEventArgs) Handles MiLogin.Authenticate
+        ConexionBBDD()
+        Login()
+    End Sub
+
+    Private Sub ConexionBBDD()
+        ':::Nuestro objeto OleDbConnetion con la cadena de conexión y la ruta de la base
+        Dim cnnString As String = String.Format("Provider=Microsoft.Jet.Oledb.4.0; Data Source=C:\Users\in2dam-b\Desktop\Urki\RetoVideoclub-ASP\App_Data\EMPRESA.mdb")
+        OleDbConnection = New OleDbConnection(cnnString)
+        ':::Utilizamos el try para capturar posibles errores
+
+        Try
+            OleDbConnection.Open()
+            If OleDbConnection.State = System.Data.ConnectionState.Open Then
+                MessageBox.Show("Se conecta")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("No se conecta")
+        Finally
+            If Not IsNothing(OleDbConnection) Then
+                OleDbConnection.Close()
+            End If
+        End Try
+    End Sub
+
+    Private Sub Login()
+        Dim usuarioEscrito As String = MiLogin.UserName
+        Dim passEscrita As String = MiLogin.Password
+        Dim sql As String = "SELECT Password FROM Clientes WHERE Nombre = '" + usuarioEscrito + "'"
+
+        Dim cmd As OleDbCommand = New OleDbCommand(sql, _OleDbConnection)
+
+        Try
+            _OleDbConnection.Open()
+            Dim pass = cmd.ExecuteScalar
+
+            If pass = passEscrita Then
+                MessageBox.Show("Login correcto")
+            Else
+                MessageBox.Show("Login incorrecto")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Conexión DB")
+        Finally
+            If Not IsNothing(OleDbConnection) Then
+                OleDbConnection.Close()
+            End If
+        End Try
 
     End Sub
 
-    Protected Sub ValidarUsuario(sender As Object, e As EventArgs)
-        Dim UserId As Integer = 0
-        Dim constr As String = ConfigurationManager.ConnectionStrings("constr").ConnectionString
-        Using cnnBD As New SqlConnection(constr)
-            Using cmd As New SqlCommand("ValidarUsuario")
-                cmd.CommandType = CommandType.StoredProcedure
-                cmd.Parameters.AddWithValue("@Username", MiLogin.UserName)
-                cmd.Parameters.AddWithValue("@Password", MiLogin.Password)
-                cmd.Connection = cnnBD
-                cnnBD.Open()
-                UserId = Convert.ToInt32(cmd.ExecuteScalar())
-                cnnBD.Close()
-            End Using
-            Select Case UserId
-                Case -1
-                    MiLogin.FailureText = "Nombre de usuario y / o contraseña incorrecta."
-                    Exit Select
-                Case -2
-                    MiLogin.FailureText = "La cuenta no ha sido activada."
-                    Exit Select
-                Case Else
-                    FormsAuthentication.RedirectFromLoginPage(MiLogin.UserName, MiLogin.RememberMeSet)
-            End Select
-        End Using
-    End Sub
+
 End Class
