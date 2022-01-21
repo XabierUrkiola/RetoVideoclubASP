32b5486fe8bd46cfb3a5fde309a89ff9c4d235d2
PabloGonzalez10 <pablo.gonzalezpr@elorrieta-errekamari.com>
2022-01-21 19:48:39 +0100

    cambiar contraseña modificado

--- RetoVideoclub-ASP/frmChangePassword.aspx
+++ RetoVideoclub-ASP/frmChangePassword.aspx
@@ -4,15 +4,24 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-            <asp:ChangePassword ID="miChangePassword" runat="server">
-            </asp:ChangePassword>
+        <div align="center">
+            Cambiar la contraseña<br /><br />
+            <asp:Label ID="LbTextContra" runat="server" Text="Contraseña:"></asp:Label>&nbsp;
+            <asp:TextBox ID="TB_Contra" runat="server"></asp:TextBox><br />
+            <asp:Label ID="LbTextNuevaContra" runat="server" Text="Nueva Contraseña:"></asp:Label>&nbsp;&nbsp;
+            <asp:TextBox ID="TB_NuevaContra" runat="server"></asp:TextBox><br />
+            <asp:Label ID="LbConfirmarContra" runat="server" Text="Confirmar la nueva contraseña:"></asp:Label>&nbsp;
+            <asp:TextBox ID="TB_ConfirmarContra" runat="server"></asp:TextBox><br />
+            Confirmar la nueva contraseña debe coincidir con la entrada Nueva contraseña<br /><br />
+            <asp:Button ID="btnCambiarContra" runat="server" Text="Cambiar contraseña" />&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnCancelar" runat="server" Text="Cancelar" />
         </div>
+
+
         <asp:Button ID="Button1" runat="server" Text="Button" />
     </form>
 </body>
--- RetoVideoclub-ASP/frmChangePassword.aspx.designer.vb
+++ RetoVideoclub-ASP/frmChangePassword.aspx.designer.vb
@@ -23,13 +23,76 @@ Partial Public Class frmChangePassword
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
     '''<summary>
-    '''Control miChangePassword.
+    '''Control LbTextContra.
     '''</summary>
     '''<remarks>
     '''Campo generado automáticamente.
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
-    Protected WithEvents miChangePassword As Global.System.Web.UI.WebControls.ChangePassword
+    Protected WithEvents LbTextContra As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control TB_Contra.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TB_Contra As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control LbTextNuevaContra.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LbTextNuevaContra As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control TB_NuevaContra.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TB_NuevaContra As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control LbConfirmarContra.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LbConfirmarContra As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control TB_ConfirmarContra.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TB_ConfirmarContra As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control btnCambiarContra.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCambiarContra As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btnCancelar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCancelar As Global.System.Web.UI.WebControls.Button
 
     '''<summary>
     '''Control Button1.
